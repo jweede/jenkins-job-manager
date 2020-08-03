5c3b5418b85473516c581e951800aedf35fe9eac
Jon Wedaman <jweede@gmail.com>
2020-08-03 14:44:04 -0400

    packing tweaks

--- jenkins_job_manager/__init__.py
+++ jenkins_job_manager/__init__.py
@@ -1,1 +1,6 @@
-__version__ = "0.1.0"
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata
+
+__version__ = importlib_metadata.version(__name__)
