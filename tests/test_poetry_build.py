d523ad91d3f3b80381fabde5ca2ceb4d715ad1fd
Jon Wedaman <jweede@gmail.com>
2021-02-05 16:36:06 -0500

    tack on a version check

--- tests/test_poetry_build.py
+++ tests/test_poetry_build.py
@@ -5,6 +5,7 @@ import re
 import pathlib
 
 import pytest
+import tomlkit
 
 here = os.path.dirname(os.path.realpath(__file__))
 repo_dir = pathlib.Path(f"{here}/../").resolve()
@@ -28,3 +29,15 @@ def test_package_install(tmp_path, pkgformat):
     subprocess.check_call(["virtualenv", "-p", sys.executable, venv_path])
     subprocess.check_call([venv_path / "bin/pip", "install", dist_dir / bname])
     subprocess.check_call([venv_path / "bin/jjm", "--help"])
+    # also check that the version matches
+    installed_version = subprocess.check_output(
+        [
+            venv_path / "bin/python",
+            "-c",
+            "from jenkins_job_manager import __version__;print(__version__)",
+        ],
+        universal_newlines=True,
+    ).strip()
+    doc = tomlkit.parse(pathlib.Path(repo_dir / "pyproject.toml").read_text())
+    toml_version = doc["tool"]["poetry"]["version"]
+    assert installed_version == toml_version
